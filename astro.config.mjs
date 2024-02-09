dbf5b7c7ddc07bf8b86a1d7c3704df9422f9d261
Yurii Habrusiev <yuriihabrusiev@users.noreply.github.com>
2024-02-09 16:10:08 +0200

    Add Spotlight integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config"
+// Import integrations
 import tailwind from "@astrojs/tailwind"
+import sentry from "@sentry/astro"
+import spotlightjs from "@spotlightjs/astro"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://habrusiev.com",
-  integrations: [tailwind()],
+  integrations: [sentry(), spotlightjs(), tailwind()],
 })
