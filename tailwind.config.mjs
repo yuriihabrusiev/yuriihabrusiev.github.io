907deb9b3a20d3c9a9ac2f368ce1cd4d3f6786a5
Yurii Habrusiev <yuriihabrusiev@users.noreply.github.com>
2024-02-10 15:32:44 +0200

    Add fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+import defaultTheme from "tailwindcss/defaultTheme"
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["'Mona Sans'", ...defaultTheme.fontFamily.sans],
+        "mona-sans": ["'Mona Sans'", ...defaultTheme.fontFamily.sans],
+        "hubot-sans": ["'Hubot Sans'", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
